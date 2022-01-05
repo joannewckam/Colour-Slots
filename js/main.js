d87ab53bfc320eb4337a6432f0d2b692b7d1acec
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-01-05 10:23:26 -0500

    add reset button and coin

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 /*----- constants -----*/
 
 
-
-
 /*----- app's state (variables) -----*/
-let reels = [0, 0, 0]
-let winCount = 0
-
+let reels
+let winCount
+let coins 
 /*----- cached element references -----*/
-let displayReel = document.getElementById('display')
+
 let leverBtn = document.getElementById('lever')
 let resetBtn = document.getElementById('reset')
 
@@ -17,6 +15,7 @@ let secondColour = document.getElementById('colour2')
 let thirdColour = document.getElementById('colour3')
 
 let wins = document.getElementById('wins')
+let coinCount = document.getElementById('coins')
 
 let winMessage = document.getElementById('win-message')
 let loseMessage = document.getElementById('lose-message')
@@ -26,27 +25,37 @@ leverBtn.addEventListener('click', handleLever)
 resetBtn.addEventListener('click', handleReset)
 
 /*----- functions -----*/
+
 initialize()
+
 function handleLever(e){
     spin()
     checkWin()
     render()
-    console.log('lever clicked!', e.target)
+    // checkLoss()
+    // console.log(coins)
+    // console.log('lever clicked!', e.target)
 }
 function handleReset(evt){
-    initialize()
-    console.log('reset clicked!', evt.target)
+    coins = 10
+    coinCount.textContent = `Coins: ${coins}`
+    leverBtn.disabled = false
+    loseMessage.style.display = 'none'
+    winCount = 0
+    wins.textContent = `Wins: ${winCount}`
+    // console.log('reset clicked!', evt.target)
 }
 function initialize(){
     reels = [0, 0, 0]
-    displayReel.style.backgroundColor = 'lightpink'
+    winCount = 0
+    coins = 10
 }
 
 function spin(){
     reels[0] = randomizer()
     reels[1] = randomizer()
     reels[2] = randomizer()
-    console.log(reels)
+    // console.log(reels)
 }
 
 function randomizer(){
@@ -59,30 +68,23 @@ function randomizer(){
 
 function render(){
     firstColour.style.backgroundColor = reels[0]
-    firstColour.innerHTML = reels[0]
     secondColour.style.backgroundColor = reels[1]
-    secondColour.innerHTML = reels[1]
     thirdColour.style.backgroundColor = reels[2]
-    thirdColour.innerHTML = reels[2]
-    
+
+    coins--
+    coinCount.textContent = `Coins: ${coins}`
+
+    if (coins === 0){
+        loseMessage.style.display = "contents"
+        // leverBtn.removeEventListener('click', handleLever)
+        leverBtn.disabled = true
+    }
 }
 
 function checkWin(){
     if (reels[0]===reels[1] && reels[0]===reels[2]){
-        console.log("you win!")
-        showWinMessage()
-        addWin()
-    }else{
-       winMessage.style.display = "none"
-    }
-}
-
-function showWinMessage(){
-    winMessage.style.display = "contents";
-}
-
-function addWin(){
-    winCount++
-    wins.textContent = `Wins: ${winCount}`
-    console.log(wins)
+        winMessage.style.display = "contents"
+        winCount++
+        wins.textContent = `Wins: ${winCount}`
+    }winMessage.style.display = "none"
 }
