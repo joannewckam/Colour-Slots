820bdf3e632081eb64771be54695f271dada27c7
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-01-06 21:55:27 -0500

    styling title and buttons

--- README.md
+++ README.md
@@ -2,52 +2,63 @@ Slot Machine Game
 
 The goal of the game is to generate three matching colours to win. Use will click the lever that will generate three random colours. User wins when three colours match. User loses when coins run out without a win.
 
+<img src="images/Start-screen.png">
+<img src="images/Win-screen.png">
+<img src="images/Loss-screen.png">
+
 TECHNOLOGIES USED:
 Javascript, HTML, CSS
 
-GETTING STARTED
+GETTING STARTED:
 <a href="https://joannewckam.github.io/firstGAproject">Demo</a>
 
 Challenges:
-assigning a colour from randomly generated number as a string
-setInterval and setTimeout to stop the generated colours at different times
-winCount went up on the first lever click because of the setTimeout - fixed by calling the checkWin function after the last spin
+
+1. assigning a colour from randomly generated number as a string
+2. setInterval and setTimeout to stop the generated colours at different times
+3. winCount went up on the first lever click because of the setTimeout - fixed by 4. calling the checkWin function after the last spin
 
 NEXT STEPS:
-add styling
-add icons to reels
-add sound effects
+
+1. add styling
+2. add icons to reels
+3. add sound effects
 
 PSEUDOCODE:
-Variables/State:
-reels
-winCount
-coins
+
+- Variables/State:
+- reels
+- winCount
+- coins
 
 Elements:
-Lever to start
-Button to reset
+
+- Lever to start
+- Button to reset
 
 Display
+
 - number of coins
 - three sets of colours
 - total wins
 
 Event Listeners
-Lever > spins the colours
-button > reset/restart
+
+- Lever > spins the colours
+- button > reset/restart
 
 GAME LOGIC:
+
 1. Three displays for the colour reels start blank
 2. User starts with 10 coins to play
 3. Click lever button to start
-    1. Display will randomly generate three colours
-        i. Both buttons disabled until the last spin
-    2. Check to see if the 3 generated colours match
-        1. If the three colours match - display win message win count goes up by 1 and user gets +5 coins
-        2. If they do not match no message will display
-        3. Coin count goes down by 1
-3. User is able to continue clicking the lever until the coins run out
-    1. When coins reach 0 a message will display that user is out of tokens to play
-    2. The lever button is disabled to prompt user to click the reset button to start again
-4. Reset button returns to initial state of 10 coins, 0 wins and white display
+   1. Display will randomly generate three colours
+      i. Both buttons disabled until the last spin
+   2. Check to see if the 3 generated colours match
+      1. If the three colours match - display win message win count goes up by 1 and user gets +5 coins
+      2. If they do not match no message will display
+      3. Coin count goes down by 1
+4. User is able to continue clicking the lever until the coins run out
+   1. When coins reach 0 a message will display that user is out of tokens to play
+   2. The lever button is disabled to prompt user to click the reset button to start again
+5. Reset button returns to initial state of 10 coins, 0 wins and white display
--- js/main.js
+++ js/main.js
@@ -1,90 +1,155 @@
 /*----- constants -----*/
-
+const drumAudioEl = new Audio("sounds/drum_roll.mp3");
+const ringAudioEl = new Audio("sounds/ring_sound.mp3");
+const winAudioEl = new Audio("sounds/win-sound.mp3");
 
 /*----- app's state (variables) -----*/
-let reels
-let winCount
-let coins 
+let reels;
+let winCount;
+let coins;
+
 /*----- cached element references -----*/
 
-let leverBtn = document.getElementById('lever')
-let resetBtn = document.getElementById('reset')
+let leverBtn = document.getElementById("lever");
+let resetBtn = document.getElementById("reset");
 
-let firstColour = document.getElementById('colour1')
-let secondColour = document.getElementById('colour2')
-let thirdColour = document.getElementById('colour3')
+let firstColour = document.getElementById("colour1");
+let secondColour = document.getElementById("colour2");
+let thirdColour = document.getElementById("colour3");
 
-let wins = document.getElementById('wins')
-let coinCount = document.getElementById('coins')
+let wins = document.getElementById("wins");
+let coinCount = document.getElementById("coins");
 
-let winMessage = document.getElementById('win-message')
-let loseMessage = document.getElementById('lose-message')
+let winMessage = document.getElementById("win-message");
+let loseMessage = document.getElementById("lose-message");
 
 /*----- event listeners -----*/
-leverBtn.addEventListener('click', handleLever)
-resetBtn.addEventListener('click', handleReset)
+leverBtn.addEventListener("click", handleLever);
+resetBtn.addEventListener("click", handleReset);
 
 /*----- functions -----*/
 
-initialize()
+initialize();
 
-function handleLever(e){
-    spin()
-    checkWin()
-    render()
-    // checkLoss()
-    // console.log(coins)
-    // console.log('lever clicked!', e.target)
-}
-function handleReset(evt){
-    coins = 10
-    coinCount.textContent = `Coins: ${coins}`
-    leverBtn.disabled = false
-    loseMessage.style.display = 'none'
-    winCount = 0
-    wins.textContent = `Wins: ${winCount}`
-    // console.log('reset clicked!', evt.target)
-}
-function initialize(){
-    reels = [0, 0, 0]
-    winCount = 0
-    coins = 10
+function handleLever() {
+	if (coins > 0) {
+		winMessage.style.display = "none";
+		loseMessage.style.display = "none";
+		resetBtn.disabled = true;
+		leverBtn.disabled = true;
+		spin();
+		spin1();
+		spin2();
+		setTimeout(function () {
+			resetBtn.disabled = false;
+			leverBtn.disabled = false;
+		}, 4000);
+	}
 }
 
-function spin(){
-    reels[0] = randomizer()
-    reels[1] = randomizer()
-    reels[2] = randomizer()
-    // console.log(reels)
+function handleReset() {
+	initialize();
+	winMessage.style.display = "none";
+	coinCount.textContent = `x ${coins}`;
+	leverBtn.disabled = false;
+	loseMessage.style.display = "none";
+	wins.textContent = `: ${winCount}`;
 }
 
-function randomizer(){
-    // return Math.floor(Math.random()*2+1)
-    let randomColour = Math.floor(Math.random()*3+1)
-    if (randomColour == 1) return 'lightblue'
-    if (randomColour == 2) return 'lightgreen'
-    if (randomColour == 3) return 'lightyellow'
+function initialize() {
+	reels = [0, 0, 0];
+
+	firstColour.style.backgroundColor = "#ffffff";
+	secondColour.style.backgroundColor = "#ffffff";
+	thirdColour.style.backgroundColor = "#ffffff";
+
+	winCount = 0;
+	coins = 10;
+	coinCount.textContent = `x ${coins}`;
+	wins.textContent = `: ${winCount}`;
 }
 
-function render(){
-    firstColour.style.backgroundColor = reels[0]
-    secondColour.style.backgroundColor = reels[1]
-    thirdColour.style.backgroundColor = reels[2]
-
-    coins--
-    coinCount.textContent = `Coins: ${coins}`
-
-    if (coins === 0){
-        loseMessage.style.display = "contents"
-        // leverBtn.removeEventListener('click', handleLever)
-        leverBtn.disabled = true
-    }
+function randomizer() {
+	let randomColour = Math.floor(Math.random() * 5 + 1);
+	if (randomColour === 1) return "#FF9AA2";
+	if (randomColour === 2) return "#FFDAC1";
+	if (randomColour === 3) return "#E2F0CB";
+	if (randomColour === 4) return "#B5EAD7";
+	if (randomColour === 5) return "#C7CEEA";
 }
 
-function checkWin(){
-    if (reels[0]===reels[1] && reels[0]===reels[2]){
-        winMessage.style.display = "contents"
-        winCount++
-        wins.textContent = `Wins: ${winCount}`
-    }winMessage.style.display = "none"
+function spin() {
+	drumAudioEl.volume = 0.03;
+	drumAudioEl.currentTime = 3.25;
+	drumAudioEl.play();
+	let spinInterval = setInterval(function () {
+		reels[0] = randomizer();
+		firstColour.style.backgroundColor = reels[0];
+	}, 200);
+	setTimeout(function () {
+		ringAudioEl.volume = 0.2;
+		ringAudioEl.currentTime = 0.25;
+		ringAudioEl.play();
+		clearInterval(spinInterval);
+	}, 1000);
+	setTimeout(function () {
+		ringAudioEl.currentTime = 0;
+		ringAudioEl.pause();
+	}, 2000);
+}
+
+function spin1() {
+	let spinInterval = setInterval(function () {
+		reels[1] = randomizer();
+		secondColour.style.backgroundColor = reels[1];
+	}, 200);
+	setTimeout(function () {
+		ringAudioEl.volume = 0.1;
+		ringAudioEl.currentTime = 0.25;
+		ringAudioEl.play();
+		clearInterval(spinInterval);
+	}, 2000);
+	setTimeout(function () {
+		ringAudioEl.currentTime = 0;
+		ringAudioEl.pause();
+	}, 4000);
+}
+
+function spin2() {
+	let spinInterval = setInterval(function () {
+		reels[2] = randomizer();
+		thirdColour.style.backgroundColor = reels[2];
+	}, 200);
+	setTimeout(function () {
+		ringAudioEl.volume = 0.1;
+		ringAudioEl.currentTime = 0.25;
+		ringAudioEl.play();
+		clearInterval(spinInterval);
+		checkWin();
+		render();
+	}, 3000);
+	setTimeout(function () {
+		ringAudioEl.currentTime = 0;
+		ringAudioEl.pause();
+	}, 4000);
+}
+
+function render() {
+	coins--;
+	coinCount.textContent = `x ${coins}`;
+	wins.textContent = `: ${winCount}`;
+	if (coins === 0) {
+		loseMessage.style.display = "block";
+	}
+}
+
+function checkWin() {
+	if (reels[0] === reels[1] && reels[0] === reels[2]) {
+		winCount++;
+		coins += 5;
+		winAudioEl.volume = 0.2;
+		winAudioEl.currentTime = 0.25;
+		winAudioEl.play();
+		winMessage.style.display = "block";
+	}
 }
